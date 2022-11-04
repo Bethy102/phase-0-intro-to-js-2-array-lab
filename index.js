1bfb00988b67e1c42be1cbaa518a8ef3955cda31
Elisabeth Aka <aea536@nyu.edu>
2022-11-04 14:08:11 -0400

    commit tree 8bd401dd70b03f1d6621e3f31594e8b9b2f979d3

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 // Write your solution here!
+const cats = ["Milo", "Otis", "Garfield"];
+function destructivelyAppendCat(name){
+    cats.push(name);
+}
+function destructivelyPrependCat(name){
+    cats.unshift(name);
+}
+function destructivelyRemoveLastCat(){
+    cats.pop();
+}
+function destructivelyRemoveFirstCat(){
+    cats.shift();
+}
